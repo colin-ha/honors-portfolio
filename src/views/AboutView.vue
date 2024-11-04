e70ea252fcd1d2fb169c20e97f04ee7a0f03b921
Colin Harris (laptop) <letsdothisgamez@gmail.com>
2024-11-03 21:20:26 -0500

    Home page + about me page completed

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,186 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import YouTubeIcon from '@/icons/YouTubeIcon.vue'
+import OneStopIcon from '@/icons/OneStopIcon.vue'
+</script>
 <template>
-  <h1>About Page</h1>
+  <div class="about-container">
+    <div class="about-row">
+      <div class="about-col">
+        <h1 class="hello">About Me</h1>
+        <p class="about-me-text">
+          I grew up in Dearborn, Michigan, later lived in Farmington Hills, and now I live in Ann Arbor for school,
+          where I’m pursuing a degree in Computer Science. I’ve always been passionate about creating and designing,
+          with no greater joy than turning ideas into reality. My dedication to careful design is driven by the belief
+          that well-designed products can improve the lives of many, and I focus on user experience and the human aspect
+          of technology. Beyond my major, I’m involved in the Wolverine Sports Analytics club, working on projects like
+          AI models to predict Wide Receiver performance, which fuels my love for learning and applying data analysis. I
+          value selflessness and strive to contribute positively to the world, embodying a "man for others" mindset. My
+          strengths include being a fast learner and embracing challenges, consistently pushing myself to grow and
+          deliver meaningful solutions that make a difference.
+        </p>
+      </div>
+      <div class="about-col quals">
+        <h1>Qualifications</h1>
+        <h2>Education</h2>
+        <p>
+          <b>The University of Michigan - Ann Arbor</b>
+          &nbsp;3.94/4.0 GPA
+          <br />
+          <em>College of Engineering & Honors Program</em>
+          <br />
+          Major in Computer Science Engineering, Minor in User Experience Design
+        </p>
+        <br />
+        <h2>Activites</h2>
+        <p>
+          <b>Wolverine Sports Analytics</b>
+          <br />
+          <em>Project Team Member</em>
+          <br />
+          Working to develop a machine learning model to predict Wide Receiver performance.
+        </p>
+        <br />
+        <h2>
+          Projects and Experiences
+          <RouterLink to="/experiences">
+            <em><span class="see-more">(see more!)</span></em>
+          </RouterLink>
+        </h2>
+        <p>
+          <YouTubeIcon class="icon" />
+          <b>Keystep Tutorials YouTube Channel</b>
+          &nbsp;
+          <a href="https://www.youtube.com/@keysteptutorials">
+            <em><span class="see-more">(check it out!)</span></em>
+          </a>
+          <br />
+          <em>Farmington Hills, MI | Nov 2021 - Present</em>
+          <br />
+          Automated Piano Tutorial videos covering a variety of songs. I wrote this platform using Java and SQLite. My
+          goal was to create tutorials for little-known songs, so that everyone can learn piano with the songs they
+          love, regardless of popularity.
+        </p>
+        <p>
+          <OneStopIcon class="icon" />
+          <b>1StopConEd LLC Website</b>
+          &nbsp;
+          <a href="https://www.1stopconed.com">
+            <em><span class="see-more">(check it out!)</span></em>
+          </a>
+          <br />
+          <em>Farmington, MI | Aug 2021 - Jun 2023</em>
+          <br />
+          Online Real Estate Education platform. Includes e-commerce, a (private) admin console, server configuration,
+          and much more. I wrote this from the ground up with Java, MySQL, Ubuntu, JavaScript, HTML, and CSS. My goal
+          was to make a seamless experience for their clients, and to provide an easy transition from their old website.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
+<style scoped>
+.about-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 90vw;
+}
+
+@media (max-width: 1040px) {
+  .about-container {
+    margin-top: 100px;
+  }
+}
+
+.about-row {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+}
+
+.about-col {
+  flex: 1;
+  min-width: 450px; /* Adjust based on your needs */
+  padding: 10px;
+}
+
+.hello {
+  font-size: 6rem;
+  background: linear-gradient(
+    45deg,
+    var(--color-gradient-start),
+    var(--color-gradient-end),
+    var(--color-gradient-hover-start),
+    var(--color-gradient-hover-end)
+  );
+  background-size: 200% 200%;
+  background-position: left center;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  margin-left: 15px;
+  transition:
+    background-position 3s ease,
+    font-weight 0.5s ease;
+  font-weight: 300;
+  text-align: center;
+}
+
+.hello:hover {
+  background-position: right center;
+  font-weight: 600;
+}
+
+p.about-me-text {
+  width: 100%;
+  padding: 0 20px;
+  font-size: larger;
+  text-align: left;
+}
+
+.see-more {
+  font-weight: normal;
+  background: none;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: initial;
+  transition:
+    background 0.5s ease,
+    font-weight 0.5s ease;
+  text-decoration: underline;
+}
+
+.see-more:hover {
+  font-weight: bolder;
+  background: linear-gradient(
+    45deg,
+    var(--color-gradient-start),
+    var(--color-gradient-end),
+    var(--color-gradient-hover-start),
+    var(--color-gradient-hover-end)
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+a:visited {
+  color: inherit;
+  text-decoration: none;
+}
+
+.icon {
+  width: 20px;
+  height: 20px;
+  vertical-align: middle;
+  margin-right: 5px;
+}
+
+.quals {
+  padding-left: 30px;
+}
 </style>
--- src/views/HonorsPlanDevelopmentView.vue
+++ src/views/HonorsPlanDevelopmentView.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-<h1>Honors Plan Development</h1>
+  <h1>Honors Plan Development</h1>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
